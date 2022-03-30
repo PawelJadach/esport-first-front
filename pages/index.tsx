4424de4e83f7f9c0a457010e918f8830e65ba8a1
Pawel Jadach <pawel.jadach@empik.com>
2022-03-30 14:31:44 +0200

    Initial project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "@components";
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            <span>Działa</span>
+        </Container>
+    );
+};
+
+export default Home;
